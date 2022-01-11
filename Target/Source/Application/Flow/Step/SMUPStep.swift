b5b33fc65f361f61fabe417aaf42c839e15c9079
baegteun <baegteun@gmail.com>
2022-01-12 00:31:25 +0900

    :zap: :: SMUP Step
    Coordinator 패턴을 적용하였기에 필요한 Step enum추가

---
+++ Target/Source/Application/Flow/Step/SMUPStep.swift
@@ -0,0 +1,34 @@
+//
+//  SMUPStep.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/01/12.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import RxFlow
+
+enum SMUPStep: Step{
+    // Global
+    case alert(title: String?, message: String?)
+    case dismiss
+    
+    // Auth
+    case signInIsRequired
+    case signInIsCompleted
+    
+    // Main
+    case mainTabbarIsRequired
+    
+    // TimeMap
+    case timeMapIsRequired
+    
+    // Chatting
+    case chattingIsRequired
+    
+    // SOOM
+    case soomIsRequired
+    
+    // Settings
+    case settingIsRequired
+}
