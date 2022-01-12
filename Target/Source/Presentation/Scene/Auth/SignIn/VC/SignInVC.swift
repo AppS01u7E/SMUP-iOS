8cba285b2adc49c298f97ad4e153b8d2f7ac4319
baegteun <baegteun@gmail.com>
2022-01-12 12:38:15 +0900

    :sparkles: :: Login UI
    Login UI 완성 // 아 브랜치 안팜ㄷㄷ

--- Target/Source/Presentation/Scene/Auth/SignIn/VC/SignInVC.swift
+++ Target/Source/Presentation/Scene/Auth/SignIn/VC/SignInVC.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 final class SignInVC: baseVC<SignInReactor>{
     // MARK: - Properties
     private let logoLabel = UILabel().then {
         $0.text = "SMUP"
+        $0.textColor = .white
+        $0.font = UIFont(font: SMUPFontFamily.NotoSansKR.black, size: 72)
+    }
+    
+    private let appleSignInButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+    
+    // MARK: - UI
+    override func addView() {
+        view.addSubViews(logoLabel, appleSignInButton)
+    }
+    override func setLayout() {
+        logoLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
         
+        appleSignInButton.snp.makeConstraints {
+            $0.top.equalTo(logoLabel.snp.bottom).offset(bound.height*0.123)
+            $0.leading.trailing.equalToSuperview().inset(bound.width*0.159)
+            $0.height.equalTo(44)
+        }
+    }
+    override func configureVC() {
+        view.backgroundColor = SMUPAsset.smupMain3.color
     }
 }
