1e79a972b50f537b37ad2e006f7c7883bdf3f086
baegteun <baegteun@gmail.com>
2022-01-14 15:55:32 +0900

    :sparkles: :: MainTabbar
    Floaty버튼을 이용한 tabbarVC 생성 사실 tabbar인지는 모르겠음 ㄹㅇㅋㅋ

--- Target/Source/Presentation/Scene/Main/Tabbar/MainTabbarVC.swift
+++ Target/Source/Presentation/Scene/Main/Tabbar/MainTabbarVC.swift
@@ -7,7 +7,74 @@
 //
 
 import UIKit
+import Floaty
+import SnapKit
+import Then
+import RxCocoa
+import RxSwift
 
 final class MainTabbarVC: UITabBarController {
+    // MARK: - Properties
+    private let floaty = Floaty().then {
+        $0.openAnimationType = .slideLeft
+        $0.buttonColor = SMUPAsset.smupFloaty.color
+        $0.itemButtonColor = SMUPAsset.smupFloaty.color
+        $0.size = 64
+    }
     
+    // MARK: - Init
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        addView()
+        setLayout()
+        configureVC()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Method
+    func setViewControllers(viewControllers: [UIViewController]? ,floaties: [FloatyItem]?, animated: Bool) {
+        self.setViewControllers(viewControllers, animated: animated)
+        guard let floaties = floaties else {
+            return
+        }
+        
+        floaties.enumerated().forEach { item in
+            item.element.handler = { _ in
+                self.didSelectFloatyItem(item.offset)
+            }
+            item.element.size = 44
+            floaty.addItem(item: item.element)
+        }
+    }
 }
+
+// MARK: - UI
+private extension MainTabbarVC{
+    func addView(){
+        self.view.insertSubview(floaty, belowSubview: self.tabBar)
+        
+    }
+    func setLayout(){
+        floaty.snp.makeConstraints {
+            $0.bottom.trailing.equalToSuperview().inset(30)
+            $0.width.height.equalTo(64)
+        }
+    }
+    func configureVC(){
+        self.tabBar.isHidden = true
+        floaty.buttonImage = UIImage(systemName: "house")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+    }
+}
+
+// MARK: - Method
+private extension MainTabbarVC{
+    func didSelectFloatyItem(_ index: Int) {
+        self.selectedIndex = index
+        self.floaty.buttonImage = self.floaty.items[index].icon
+    }
+}
+
