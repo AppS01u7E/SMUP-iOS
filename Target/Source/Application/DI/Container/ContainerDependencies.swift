8e030b04c38facfd3b9e4cd89e7da2d92906be93
baegteun <baegteun@gmail.com>
2022-01-14 15:56:41 +0900

    :sparkles: :: Dependency Inject
    만들어진 모든 VC, Reactor, Flow, Stepper를 의존성주입.

--- Target/Source/Application/DI/Container/ContainerDependencies.swift
+++ Target/Source/Application/DI/Container/ContainerDependencies.swift
@@ -8,6 +8,7 @@
 
 import Swinject
 import SwinjectAutoregistration
+import RxFlow
 
 extension Container{
     func registerDependencies() {
@@ -19,29 +20,41 @@ extension Container{
     
     // MARK: - VC
     private func registerVC() {
-        [
-            SignInVC.self
-        ].forEach{ autoregister($0, initializer: $0.init) }
+        autoregister(SignInVC.self, initializer: SignInVC.init)
+        autoregister(HomeVC.self, initializer: HomeVC.init)
+        autoregister(TimeMapVC.self, initializer: TimeMapVC.init)
+        autoregister(ChattingVC.self, initializer: ChattingVC.init)
+        autoregister(SOOMVC.self, initializer: SOOMVC.init)
+        autoregister(SettingVC.self, initializer: SettingVC.init)
     }
     
     // MARK: - Reactor
     private func registerReactor() {
-        [
-            SignInReactor.self
-        ].forEach{ autoregister($0, initializer: $0.init) }
+        autoregister(SignInReactor.self, initializer: SignInReactor.init)
+        autoregister(HomeReactor.self, initializer: HomeReactor.init)
+        autoregister(TimeMapReactor.self, initializer: TimeMapReactor.init)
+        autoregister(ChattingReactor.self, initializer: ChattingReactor.init)
+        autoregister(SOOMReactor.self, initializer: SOOMReactor.init)
+        autoregister(SettingReactor.self, initializer: SettingReactor.init)
     }
     
     // MARK: - Stepper
     private func registerStepper() {
-        [
-            SignInStepper.self
-        ].forEach{ autoregister($0, initializer: $0.init) }
+        autoregister(SignInStepper.self, initializer: SignInStepper.init)
+        autoregister(HomeStepper.self, initializer: HomeStepper.init)
+        autoregister(TimeMapStepper.self, initializer: TimeMapStepper.init)
+        autoregister(ChattingStepper.self, initializer: ChattingStepper.init)
+        autoregister(SOOMStepper.self, initializer: SOOMStepper.init)
+        autoregister(SettingStepper.self, initializer: SettingStepper.init)
     }
     
     // MARK: - Flow
     private func registerFlow() {
-        [
-            SignInFlow.self
-        ].forEach{ autoregister($0, initializer: $0.init) }
+        autoregister(SignInFlow.self, initializer: SignInFlow.init)
+        autoregister(HomeFlow.self, initializer: HomeFlow.init)
+        autoregister(TimeMapFlow.self, initializer: TimeMapFlow.init)
+        autoregister(ChattingFlow.self, initializer: ChattingFlow.init)
+        autoregister(SOOMFlow.self, initializer: SOOMFlow.init)
+        autoregister(SettingFlow.self, initializer: SettingFlow.init)
     }
 }
