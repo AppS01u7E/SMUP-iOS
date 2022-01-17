bc06bd94d55d3243d926b99e4bfd098c83a9f068
baegteun <baegteun@gmail.com>
2022-01-17 21:30:49 +0900

    :sparkles: :: TimeMpaDetailCellFile

--- Target/Source/Presentation/Scene/Main/Items/TimeMapDetail/VC/TimeMapDetailVC.swift
+++ Target/Source/Presentation/Scene/Main/Items/TimeMapDetail/VC/TimeMapDetailVC.swift
@@ -11,6 +11,11 @@ import UIKit
 
 final class TimeMapDetailVC: baseVC<TimeMapDetailReactor>{
     // MARK: - Properties
+    private let transparentView = UIView().then {
+        $0.backgroundColor = .black
+        $0.alpha = 0.1
+    }
+    
     private let timeMapsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
     
     // MARK: - Init
@@ -24,5 +29,16 @@ final class TimeMapDetailVC: baseVC<TimeMapDetailReactor>{
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+    override func addView() {
+        
+    }
+    override func setLayout() {
+        
+    }
+    override func configureVC() {
+        view.backgroundColor = .clear
     }
 }
---
+++ Target/Source/Presentation/Scene/Main/Items/TimeMapDetail/View/TimeMapDetailCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimeMapDetailCell.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/01/17.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import UIKit
+
+final class TimeMapDetailCell: baseCollectionViewCell<TimeMap>{
+    // MARK: - Properties
+    
+}
