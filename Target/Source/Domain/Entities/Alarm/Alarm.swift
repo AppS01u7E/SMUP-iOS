b17c589cd02b0bde04181f658c14990ac62589e6
baegteun <baegteun@gmail.com>
2022-01-19 13:15:00 +0900

    :sparkles: ::

---
+++ Target/Source/Domain/Entities/Alarm/Alarm.swift
@@ -0,0 +1,22 @@
+//
+//  Alarm.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/01/19.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import RxDataSources
+import Foundation
+
+struct Alarm: ModelType{
+    let id: String
+    let content: String
+    let date: Date
+}
+
+extension Alarm: IdentifiableType{
+    var identity: String{
+        return UUID().uuidString
+    }
+}
---
+++ Target/Source/Presentation/Scene/Common/Alarm/VIew/AlarmCell.swift
@@ -0,0 +1,11 @@
+//
+//  AlarmCell.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/01/19.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import UIKit
+
+final class AlarmCell: baseTableViewCell<
