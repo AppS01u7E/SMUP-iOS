9cf526a839dcc594dcceba8a32bb6ff14df55341
baegteun <baegteun@gmail.com>
2022-01-19 16:25:29 +0900

    :sparkles: :: SoomCategorySection

--- Target/Source/Presentation/Scene/Soom/Items/Search/VC/SearchVC.swift
+++ Target/Source/Presentation/Scene/Soom/Items/Search/VC/SearchVC.swift
@@ -6,8 +6,51 @@
 //  Copyright © 2022 baegteun. All rights reserved.
 //
 
+import UIKit
+
 final class SearchVC: baseVC<SearchReactor>{
     // MARK: - Properties
     private let searchTextField = SearchTextField(placeholder: "검색하실 SOOM 이름을 입력해주세요.")
+    private let majorCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        $0.register(SoomCategoryCollectionViewCell.self, forCellWithReuseIdentifier: SoomCategoryCollectionViewCell.reusableID)
+        $0.collectionViewLayout = layout
+    }
     
+    // MARK: - UI
+    override func setUp() {
+        majorCollectionView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+    override func addView() {
+        view.addSubViews(searchTextField, majorCollectionView)
+    }
+    override func setLayout() {
+        searchTextField.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(bound.height*0.023)
+            $0.leading.trailing.equalToSuperview().inset(bound.width*0.11)
+            $0.height.equalTo(40)
+        }
+        majorCollectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(searchTextField.snp.bottom).offset(13)
+        }
+    }
+    override func configureVC() {
+        
+    }
+    
+    // MARK: - Reactor
+    override func bindAction(reactor: SearchReactor) {
+       
+    }
+}
+
+// MARK: - Extension
+extension SearchVC: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: bound.width*0.2294, height: bound.width*0.2294)
+    }
 }
--- Target/Source/Presentation/Scene/Soom/Items/Search/View/SoomCategoryCollectionViewCell.swift
+++ Target/Source/Presentation/Scene/Soom/Items/Search/View/SoomCategoryCollectionViewCell.swift
@@ -7,10 +7,43 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class SoomCategoryCollectionViewCell: baseCollectionViewCell<SoomCategory>{
     // MARK: - Properties
-    private let view = UIView()
-    private let majorLabel = UILabel()
+    private let majorLabel = UILabel().then {
+        $0.font = UIFont(font: SMUPFontFamily.Inter.medium, size: 12)
+        $0.textColor = .white
+    }
+    private let iconImageView = UIImageView()
     
+    // MARK: - UI
+    override func addView() {
+        addSubViews(majorLabel, iconImageView)
+    }
+    override func setLayout() {
+        majorLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(14)
+        }
+        iconImageView.snp.makeConstraints {
+            $0.bottom.trailing.equalToSuperview().inset(7)
+        }
+    }
+    override func bind(_ model: SoomCategory) {
+        self.backgroundColor = model.color
+        majorLabel.text = model.major.rawValue
+        var image: UIImage = UIImage()
+        switch model.major{
+        case .web:
+            image = UIImage(systemName: "pc") ?? .init()
+        case .app:
+            image = UIImage(systemName: "iphone") ?? .init()
+        case .backEnd:
+            image = UIImage(systemName: "server.rack") ?? .init()
+        case .game:
+            image = UIImage(systemName: "gamecontroller") ?? .init()
+        }
+        iconImageView.image = image.tintColor(.white).downSample(size: .init(width: 12, height: 12))
+    }
 }
