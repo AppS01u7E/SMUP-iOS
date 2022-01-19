017fb3284c9dda6621bd8eee36fdbe08da2d8577
baegteun <baegteun@gmail.com>
2022-01-19 13:18:57 +0900

    :sparkles: :: AlarmCell

--- Target/Source/Presentation/Scene/Common/Alarm/VIew/AlarmCell.swift
+++ Target/Source/Presentation/Scene/Common/Alarm/VIew/AlarmCell.swift
@@ -7,7 +7,30 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class AlarmCell: baseTableViewCell<Alarm>{
+    // MARK: - Properties
+    private let contentLabel = UILabel()
+    private let dateLabel = UILabel()
     
+    // MARK: - UI
+    override func addView() {
+        addSubViews(contentLabel, dateLabel)
+    }
+    override func setLayout() {
+        dateLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(5)
+        }
+        contentLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(5)
+            $0.trailing.equalTo(dateLabel.snp.leading).priority(.low)
+        }
+    }
+    override func configureCell() {
+        
+    }
 }
