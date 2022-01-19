6c86db169537f2b707c85d58662adaa90d4ce793
baegteun <baegteun@gmail.com>
2022-01-19 16:39:45 +0900

    :sparkles: :: Soom Model

---
+++ Target/Source/Domain/Entities/Soom/Soom.swift
@@ -0,0 +1,14 @@
+//
+//  Soom.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/01/19.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+struct Soom: ModelType{
+    let name: String
+    let description: String
+}
