d7be56155ac9f7fd601dbe03ddc40cef8e1f1ba4
baegteun <baegteun@gmail.com>
2022-01-30 16:46:41 +0900

    :sparkles: :: Schedule Text

--- Target/Source/Presentation/Utils/Extensions/Swift/Date/DateExt.swift
+++ Target/Source/Presentation/Utils/Extensions/Swift/Date/DateExt.swift
@@ -24,37 +24,8 @@ extension Date{
     }
     
     func convertSchedule() -> String{
-        var str = "\(self.convertKorea().toString(.custom("a hh시 mm분")))\n"
-        if self.resetDate().isInRange(date: 1.convertStartTime().toHour() - 10.minutes, and: 1.convertStartTime().toHour()){
-            str.append("곧 1교시 수업이 시작됩니다")
-        }
-        else if self.resetDate().isInRange(date: 1.convertStartTime().toHour(), and: 1.convertEndTime().toHour()){
-            str.append("지금은 1교시 수업중")
-        }
-        else if self.resetDate().isInRange(date: 2.convertStartTime().toHour() - 10.minutes, and: 2.convertStartTime().toHour()){
-            str.append("곧 2교시 수업이 시작됩니다")
-        }
-        else if self.resetDate().isInRange(date: 2.convertStartTime().toHour(), and: 2.convertEndTime().toHour()){
-            str.append("지금은 2교시 수업중")
-        }
-        else if self.resetDate().isInRange(date: 3.convertStartTime().toHour() - 10.minutes, and: 3.convertStartTime().toHour()){
-            str.append("곧 2교시 수업이 시작됩니다")
-        }
-        else if self.resetDate().isInRange(date: 3.convertStartTime().toHour(), and: 3.convertEndTime().toHour()){
-            str.append("지금은 3교시 수업중")
-        }
-        else if self.resetDate().isInRange(date: 4.convertStartTime().toHour() - 10.minutes, and: 4.convertStartTime().toHour()){
-            str.append("곧 4교시 수업이 시작됩니다")
-        }
-        else if self.resetDate().isInRange(date: 4.convertStartTime().toHour(), and: 4.convertEndTime().toHour()){
-            str.append("지금은 4교시 수업중")
-        }
-        else if self.resetDate().isInRange(date: 5.convertStartTime().toHour(), and: 5.convertEndTime().toHour()){
-            str.append("지금은 5교시 수업중")
-        }
-        else{
-            str.append("지금은 n교시 수업중")
-        }
+        var str = "\(self.toString(.custom("a hh시 mm분")))\n"
+        str.append("곧 n교시 수업이 시작됩니다")
         return str
     }
     
