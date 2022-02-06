ff2ade6242685161711db5fa08e84227d0e01e70
baegteun <baegteun@gmail.com>
2022-02-07 01:20:10 +0900

    :sparkles: :: alreadyJoined추가. notFound 통합

--- Target/Source/Domain/Error/SMUPError.swift
+++ Target/Source/Domain/Error/SMUPError.swift
@@ -18,11 +18,12 @@ enum SMUPError: Error {
     case wrongRefreshToken
     
     // MARK: group
-    case groupNotFound
+    case notFound
     case pagenationPolicyViolation
     case groupTypePolicyViolation
     case permisionDenid
     case idPolicyViolation
+    case alreadyJoined
 }
 
 extension SMUPError: LocalizedError {
@@ -34,8 +35,8 @@ extension SMUPError: LocalizedError {
             return "권한이 없습니다."
         case .wrongRefreshToken:
             return "refresh토큰이 잘못되었습니다."
-        case .groupNotFound:
-            return "그룹을 찾지 못하였습니다."
+        case .notFound:
+            return "request에 해당하는 것을 찾지 못하였습니다."
         case .pagenationPolicyViolation:
             return "idx나 size가 자연수가 아닙니다."
         case .groupTypePolicyViolation:
@@ -44,6 +45,8 @@ extension SMUPError: LocalizedError {
             return "해당 그룹의 type을 수정할 권한이 없습니다."
         case .idPolicyViolation:
             return "ID가 자연수가 아닙니다."
+        case .alreadyJoined:
+            return "이미 그룹에 가입되어있습니다."
         }
     }
 }
