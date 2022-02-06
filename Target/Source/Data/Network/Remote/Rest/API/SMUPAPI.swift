902442b9eb2b3b46b866bc1322bbd52f44229f26
baegteun <baegteun@gmail.com>
2022-02-06 17:42:33 +0900

    :sparkles: :: SMUP_API

---
+++ Target/Source/Data/Network/Remote/Rest/API/SMUPAPI.swift
@@ -0,0 +1,52 @@
+//
+//  SMUPAPI.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/06.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol SMUPAPI: TargetType, JWTTokenAuthorizable {
+    var domain: SMUPDomain { get }
+    var urlPath: String { get }
+    var errorMapper: [Int: SMUPError]? { get }
+}
+
+extension SMUPAPI {
+    var baseURL: URL {
+        return URL(string: "")!
+    }
+    
+    var path: String {
+        return domain.url + urlPath
+    }
+    
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var validationType: ValidationType {
+        return .successCodes
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
+}
+
+enum SMUPDomain: String {
+    case auth
+}
+
+extension SMUPDomain {
+    var url: String {
+        return "/\(self.rawValue)"
+    }
+}
