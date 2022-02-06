f05753edd3bc8b4c25b15d3a70be6f053093ff04
baegteun <baegteun@gmail.com>
2022-02-06 17:24:24 +0900

    :sparkles: :: TokenError

---
+++ Target/Source/Domain/Error/TokenError.swift
@@ -0,0 +1,12 @@
+//
+//  TokenError.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/06.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+enum TokenError: Error {
+    case tokenExpired
+    case noToken
+}
