14ede58cef73a5ee526d37fd53e1c93e2cea87ce
baegteun <baegteun@gmail.com>
2022-02-06 17:34:14 +0900

    :sparkles: :: SMUPError

---
+++ Target/Source/Domain/Error/SMUPError.swift
@@ -0,0 +1,16 @@
+//
+//  SMUPError.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/06.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+// MARK: - SMUPError
+enum SMUPError: Error {
+    // MARK: Base
+    case noInternet
+    case unauthorization
+    
+    
+}
