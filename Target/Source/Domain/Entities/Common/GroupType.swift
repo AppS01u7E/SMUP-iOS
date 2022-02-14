dea30a25f64fe05f1dfc3af4b08de9bed497ca1c
baegteun <baegteun@gmail.com>
2022-02-14 21:49:19 +0900

    :sparkles: :: Common Entities
    School, GroupType

---
+++ Target/Source/Domain/Entities/Common/GroupType.swift
@@ -0,0 +1,16 @@
+//
+//  GroupType.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/14.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+enum GroupType: String {
+    case counsil = "COUNSIL"
+    case clubMajor = "CLUB_MAJOR"
+    case clubCa = "CLUB_CA"
+    case clubEtc = "CLUB_ETC"
+    case team = "TEAM"
+    case `class` = "CLASS"
+}
---
+++ Target/Source/Domain/Entities/Common/School.swift
@@ -0,0 +1,13 @@
+//
+//  School.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/14.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+enum School: String {
+    case busan = "BUSAN"
+    case daedeok = "DAEDEOK"
+    case gwangju = "GWANGJU"
+}
