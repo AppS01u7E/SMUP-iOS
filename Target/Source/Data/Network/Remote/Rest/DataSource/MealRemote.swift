6f88d689b7458ed914d779415b2792214eda394b
baegteun <baegteun@gmail.com>
2022-02-16 23:31:52 +0900

    :sparkles: :: MealRemote

---
+++ Target/Source/Data/Network/Remote/Rest/DataSource/MealRemote.swift
@@ -0,0 +1,23 @@
+//
+//  MealRemote.swift
+//  SMUP
+//
+//  Created by 최형우 on 2022/02/16.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import RxSwift
+import Foundation
+
+final class MealRemote: BaseRemote<MealAPI> {
+    static let shared = MealRemote()
+    
+    func getMeal(date: Date) -> Single<Meal> {
+        return request(.getMeal(date))
+            .map(Meal.self)
+    }
+    func getTodayMeal() -> Single<Meal> {
+        return request(.getTodayMeal)
+            .map(Meal.self)
+    }
+}
