f4690db8882137596d34d127a1dcd1a4308b9750
baegteun <baegteun@gmail.com>
2022-02-16 23:00:28 +0900

    :sparkles: :: Error Mapper update

--- Target/Source/Domain/Error/SMUPError.swift
+++ Target/Source/Domain/Error/SMUPError.swift
@@ -12,18 +12,10 @@ import Foundation
 enum SMUPError: Error {
     // MARK: Base
     case noInternet
+    
     case unauthorization
-    
-    // MARK: signin
-    case wrongRefreshToken
-    
-    // MARK: group
+    case forbidden
     case notFound
-    case pagenationPolicyViolation
-    case groupTypePolicyViolation
-    case permisionDenid
-    case idPolicyViolation
-    case alreadyJoined
 }
 
 extension SMUPError: LocalizedError {
@@ -33,20 +25,10 @@ extension SMUPError: LocalizedError {
             return "인터넷이 연결되어있지 않습니다."
         case .unauthorization:
             return "권한이 없습니다."
-        case .wrongRefreshToken:
-            return "refresh토큰이 잘못되었습니다."
+        case .forbidden:
+            return "활성화된 accessToken을 찾을 수 없습니다."
         case .notFound:
-            return "request에 해당하는 것을 찾지 못하였습니다."
-        case .pagenationPolicyViolation:
-            return "idx나 size가 자연수가 아닙니다."
-        case .groupTypePolicyViolation:
-            return "Group Type이 정책을 위반합니다."
-        case .permisionDenid:
-            return "해당 그룹의 type을 수정할 권한이 없습니다."
-        case .idPolicyViolation:
-            return "ID가 자연수가 아닙니다."
-        case .alreadyJoined:
-            return "이미 그룹에 가입되어있습니다."
+            return "결과를 찾지 못하였습니다."
         }
     }
 }
